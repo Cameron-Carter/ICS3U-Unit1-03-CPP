01c58f8ceb2cc7d19a1f43c7f425dee87dfd42e0
Cameron Carter <cameron.carter@mths.ca>
2021-04-23 14:17:09 +0000

    made hello world program

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) Cameron Carter All rights reserved
+//
+// Created by Cameron Carter
+// Created on April 2021
+// This is a simple "Hello, World!" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
